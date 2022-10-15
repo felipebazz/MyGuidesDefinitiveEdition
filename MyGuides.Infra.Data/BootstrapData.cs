79385ead30fbcab6c9b7ad1b7bd2dda46d9e986c
Felipe Morais <felipebazz@live.com>
2022-10-14 23:38:09 -0300

    Implementado UnitOfWork

--- MyGuides.Infra.Data/BootstrapData.cs
+++ MyGuides.Infra.Data/BootstrapData.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MyGuides.Infra.Data.Contexts.Database;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyGuides.Infra.Data
 {
---
+++ MyGuides.Infra.Data/Contexts/Database/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MyGuides.Infra.Data.Contexts.Database
+{
+    [ExcludeFromCodeCoverage]
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly MyGuidesContext _dbContext;
+
+        public UnitOfWork(MyGuidesContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task SaveChangesAsync(CancellationToken cancellationToken) => _dbContext.SaveChangesAsync(cancellationToken);
+        
+    }
+}
