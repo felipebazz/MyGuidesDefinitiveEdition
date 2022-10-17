3fcbd8c76f6fef3a9f42378b1ea1d46a84e401a1
Felipe Morais <felipebazz@live.com>
2022-10-17 17:44:15 -0300

    Implementado projeto de startup

---
+++ MyGuides.Domain/Registrations/BootstrapDomain.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace MyGuides.Domain.Registrations
+{
+    public static class BootstrapDomain
+    {
+        public static IServiceCollection RegistarDomain(this IServiceCollection service)
+        {
+            service.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return service;
+        }
+    }
+}
---
+++ MyGuides.Startup/BootstrapStartup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using MyGuides.Application.Registrations;
+using MyGuides.Domain.Registrations;
+using MyGuides.Infra.Data.Registrations;
+using MyGuides.Notifications.Registrations;
+
+
+namespace MyGuides.Startup
+{
+    public static class BootstrapStartup
+    {
+        public static void RegisterStartup(this WebApplicationBuilder builder)
+        {
+            AddBootstrapMyGuides(builder);
+        }
+
+        public static void AddBootstrapMyGuides(WebApplicationBuilder builder)
+        {
+            builder.Services.RegistarDomain();
+            builder.Services.RegisterData(builder.Configuration);
+            builder.Services.RegisterNotifications();
+            builder.Services.RegistarApplication();
+        }
+    }
+}
