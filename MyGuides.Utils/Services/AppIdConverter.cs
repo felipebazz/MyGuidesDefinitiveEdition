daf885971632ca66bcb0330833c4b0aaf9c2b4ed
Felipe Morais <felipebazz@live.com>
2022-10-26 02:55:26 -0300

    Implementação do projeto de utilidades

---
+++ MyGuides.Utils/Services/AppIdConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace MyGuides.Utils
+{
+    public static class AppIdConverter
+    {
+        public static string GetAppId(string appId)
+        {
+            List<string> url = appId.Split('/').ToList();
+            if (url != null && url.Count() > 0)
+            {
+                foreach (var item in url)
+                {
+                    if (Utils.IsNumeric(item))
+                    {
+                        appId = item;
+                        break;
+                    }
+                }
+            }
+
+            return appId;
+        }
+    }
+}
---
+++ MyGuides.Utils/Services/Utils.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MyGuides.Utils
+{
+    public static class Utils
+    {
+        public static bool IsNumeric(string param)
+        {
+            if (Regex.IsMatch(param, @"^\d+$"))
+                return true;
+            else
+                return false;
+        }
+
+        public static string Capitalize(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return string.Empty;
+
+            string realName = string.Empty;
+            input = input.ToLower();
+            List<string> word = input.Split(' ').ToList();
+
+            if (word.Count() > 1)
+            {
+                foreach (var item in word)
+                {
+                    if (IsNumeric(item))
+                    {
+                        realName += item + " ";
+                    }
+                    else
+                    {
+                        realName += char.ToUpper(item[0]) + item.Substring(1) + " ";
+                    }
+                }
+            }
+            else
+                realName = char.ToUpper(input[0]) + input.Substring(1);
+
+            realName = realName.Trim();
+            return realName;
+        }
+    }
+}
